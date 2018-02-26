a83740ffafd20c7ab329b7894a98f6f5d5d5df4e
PearsonLawrence <pearson.lawrence@students.aie.edu.au>
2018-02-25 19:38:05 -0800

    init

---
+++ Assets/Programming/Pearson/_Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class CameraController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public bool isDown;
+    private float downTime;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.isDown = true;
+        this.downTime = Time.realtimeSinceStartup;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.isDown = false;
+    }
+
+    public void Update()
+    {
+        if (!this.isDown) return;
+        if (Time.realtimeSinceStartup - this.downTime > 2f)
+        {
+            print("Handle Long Tap");
+            this.isDown = false;
+        }
+    }
+}
